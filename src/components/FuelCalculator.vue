75d6e2029cb91ab7827705670498be31a030cd8b
Julian Grey <julianrgrey@gmail.com>
2025-03-23 17:50:41 +0000

    Add basic fuel calculations, remove initial Vue files

---
+++ src/components/FuelCalculator.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const fuelPerLap = ref(0)
+const lapTimeInSeconds = ref(0)
+const raceDurationInMinutes = ref(0)
+const fuelNeeded = ref(0)
+const numberOfLaps = ref(0)
+
+const calculateNumberOfLaps = () => {
+  numberOfLaps.value = Math.ceil((raceDurationInMinutes.value * 60) / lapTimeInSeconds.value)
+}
+
+const calculateFuelNeeded = () => {
+  fuelNeeded.value =
+    Math.ceil((raceDurationInMinutes.value * 60) / lapTimeInSeconds.value) * fuelPerLap.value
+}
+</script>
+
+<template>
+  <div class="fuel-calculator">
+    <div class="fuel-per-lap">
+      <label for="fuelPerLap">Fuel per lap: </label>
+      <input type="number" v-model="fuelPerLap" />
+      <p>{{ fuelPerLap }}</p>
+    </div>
+    <div class="lap-time-in-seconds">
+      <label for="lapTimeInSeconds">Lap time in seconds: </label>
+      <input type="number" v-model="lapTimeInSeconds" />
+      <p>{{ lapTimeInSeconds }}</p>
+    </div>
+    <div class="race-duration">
+      <label for="raceDurationInMinutes">Race duration: </label>
+      <input type="number" v-model="raceDurationInMinutes" />
+      <p>{{ raceDurationInMinutes }}</p>
+    </div>
+    <div class="number-of-laps">
+      <button @click="calculateNumberOfLaps">Calculate number of laps</button>
+      <p>{{ numberOfLaps }}</p>
+    </div>
+    <div class="fuel-needed">
+      <button @click="calculateFuelNeeded">Calculate fuel needed</button>
+      <p>{{ fuelNeeded }}</p>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import FuelCalculator from '@/components/FuelCalculator.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <FuelCalculator />
   </main>
 </template>
