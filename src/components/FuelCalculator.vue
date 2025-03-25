acdef35a6f60097da11c4a21d4052027c2e263d1
Julian Grey <julianrgrey@gmail.com>
2025-03-25 11:03:51 +0000

    Slight reorganisation of markdown

--- src/components/FuelCalculator.vue
+++ src/components/FuelCalculator.vue
@@ -19,28 +19,29 @@ const calculateFuelNeeded = () => {
 
 <template>
   <div class="fuel-calculator">
-    <div class="fuel-per-lap">
-      <label for="fuelPerLap">Fuel per lap: </label>
-      <input type="number" v-model="fuelPerLap" />
-      <p>{{ fuelPerLap }}</p>
+    <div class="calculation-inputs">
+      <div class="fuel-per-lap">
+        <label for="fuelPerLap">Fuel per lap: </label>
+        <input type="number" v-model="fuelPerLap" />
+      </div>
+      <div class="lap-time-in-seconds">
+        <label for="lapTimeInSeconds">Lap time in seconds: </label>
+        <input type="number" v-model="lapTimeInSeconds" />
+      </div>
+      <div class="race-duration">
+        <label for="raceDurationInMinutes">Race duration: </label>
+        <input type="number" v-model="raceDurationInMinutes" />
+      </div>
+      <div class="number-of-laps">
+        <button @click="calculateNumberOfLaps">Calculate number of laps</button>
+      </div>
+      <div class="fuel-needed">
+        <button @click="calculateFuelNeeded">Calculate fuel needed</button>
+      </div>
     </div>
-    <div class="lap-time-in-seconds">
-      <label for="lapTimeInSeconds">Lap time in seconds: </label>
-      <input type="number" v-model="lapTimeInSeconds" />
-      <p>{{ lapTimeInSeconds }}</p>
-    </div>
-    <div class="race-duration">
-      <label for="raceDurationInMinutes">Race duration: </label>
-      <input type="number" v-model="raceDurationInMinutes" />
-      <p>{{ raceDurationInMinutes }}</p>
-    </div>
-    <div class="number-of-laps">
-      <button @click="calculateNumberOfLaps">Calculate number of laps</button>
-      <p>{{ numberOfLaps }}</p>
-    </div>
-    <div class="fuel-needed">
-      <button @click="calculateFuelNeeded">Calculate fuel needed</button>
-      <p>{{ fuelNeeded }}</p>
+    <div class="calculation-results">
+      <p>Number of laps: {{ numberOfLaps }}</p>
+      <p>Fuel needed: {{ fuelNeeded }}</p>
     </div>
   </div>
 </template>
